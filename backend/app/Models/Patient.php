8db73913b6596507893c6b3fd7dd01d4d5144888
Bintang Jati <bintangid69@gmail.com>
2026-06-02 11:40:39 +0700

    ganti id pasien dari int ke str

--- backend/app/Models/Patient.php
+++ backend/app/Models/Patient.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Patient extends Model
 {
     //
+    public $incrementing = false;
+    protected $keyType = 'string';
+    
+    // Tambahkan baris ini agar semua kolom (termasuk ID) diizinkan untuk diisi
+    protected $guarded = [];
 }
